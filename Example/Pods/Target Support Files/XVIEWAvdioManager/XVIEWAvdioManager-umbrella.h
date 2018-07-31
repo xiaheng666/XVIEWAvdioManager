e2174101a8caf8d858f2576c8260874882af5939
yyj <yyj@Air.local>
2018-07-31 14:06:15 +0800

    XXX

---
+++ Example/Pods/Target Support Files/XVIEWAvdioManager/XVIEWAvdioManager-umbrella.h
@@ -0,0 +1,27 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AudioPlayerManager.h"
+#import "AudioProcessManager.h"
+#import "AudioRecordView.h"
+#import "BBHoldToSpeakButton.h"
+#import "BBVoiceRecordController.h"
+#import "BBVoiceRecordHeaderDefine.h"
+#import "BBVoiceRecordPowerAnimationView.h"
+#import "BBVoiceRecordToastContentView.h"
+#import "BBVoiceRecordView.h"
+#import "UIColor+BBVoiceRecord.h"
+#import "UIImage+BBVoiceRecord.h"
+
+FOUNDATION_EXPORT double XVIEWAvdioManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char XVIEWAvdioManagerVersionString[];
+
